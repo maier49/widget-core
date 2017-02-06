079549b868980f2bc3d33e2f540740e2b791990a
Bradley Maier <bmaier@sitepen.com>
2017-02-06 15:52:41 -0500

    Fix paths for i18n on windows in tests

--- tests/support/nls/greetings.ts
+++ tests/support/nls/greetings.ts
@@ -3,8 +3,10 @@ import has from '@dojo/has/has';
 // The default loader attempts to use the native Node.js `require` when running on Node. However, the Intern
 // suite uses the @dojo/loader, in which case the context for requires is the location of the loader module; or in
 // this case, `node_modules/@dojo/loader/loader.min.js'.
-const basePath = has('host-node') ? '../../_build/' : '';
-const bundlePath = `${basePath}tests/support/nls/greetings`;
+const hasHostNode = has('host-node');
+const pathSeparator = hasHostNode ? require('path').sep : '/';
+const basePath =  hasHostNode ? `..${pathSeparator}..${pathSeparator}_build${pathSeparator}` : '';
+const bundlePath = `${basePath}tests${pathSeparator}support${pathSeparator}nls${pathSeparator}greetings`;
 const locales = [ 'fr' ];
 
 const messages = {
